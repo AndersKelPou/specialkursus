1f3976c8673f73e5ac7be88afa3b70354aa670dd
Anders Poulsen <s204423@student.dtu.dk>
2023-03-11 11:41:42 +0100

    ya mom part 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { Variable } from "./Variable"
+
+//Ideas:
+//One of the inputs are dedicated to decide how many variables are in the term
+//Select that many variable names from an array with possibles.
+//Create a recursive function that will follow the rules of lambda terms
+//Maybe also have a variable with minimum length?
+
 
 function App() {
+  const [ result, setResult ] = useState('');
+  const [ variable1, setVariable1 ] = useState(2);
+
+  const testSetter = () => setResult("This is a lambda term");
+  const variableTest = (update) => {
+    setVariable1(update);
+    setResult(update);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="generator-container">
+        <div className="headline">
+          <h1>Lambda Term Generator</h1>
+        </div>
+        <div className="divElement">
+          <input type="text" className="element" readOnly value={result}/>
+        </div>
+        <Variable label="Some Variable" value={variable1} onChange={variableTest} />
+        <div className="divElement">
+          <button onClick={testSetter} className="element">Click</button>
+        </div>
+      </div>
     </div>
   );
 }
