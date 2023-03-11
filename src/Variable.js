463dba163a97c7aa419f282548792cacbf702355
Anders Poulsen <s204423@student.dtu.dk>
2023-03-11 11:41:19 +0100

    ya mom

---
+++ src/Variable.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+export class Variable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        const value = e.target.value;
+        this.props.onChange(value);
+    }
+
+    render() {
+        return (
+        <div className="divElement">
+          <label>{this.props.label}</label>
+          <input type="text" className="variable-input" defaultValue={this.props.value} onChange={this.handleChange}/>
+        </div>
+        );
+    }
+}
